779f579d047dd4d99b293ba76b16bdea26b0cf57
Leidus-Cassio <lordivanent@gmail.com>
2023-09-13 12:31:31 -0300

    Light/Dark  mode add, hover changed, switch button icon added, blur background added

--- script.js
+++ script.js
@@ -6,11 +6,11 @@ function toggleMode() {
     const img = document.querySelector("#profile img")
    
      if(html.classList.contains('Light')) {
-       img.setAttribute("src", "assets/profile-dark.png")
-       img.setAttribute("alt", "Foto de Leidus fundo escuro")
-       
-    } else {
        img.setAttribute("src", "assets/profile-light.png")
        img.setAttribute("alt", "Foto de Leidus fundo claro")
+       
+    } else {
+       img.setAttribute("src", "assets/profile-dark.png")
+       img.setAttribute("alt", "Foto de Leidus fundo escuro")
     }
 }
